154da15789def3659f3657e876d29edf815c3e9b
Rebecca-alu <r.benegusen@alustudent.com>
2026-06-03 07:36:07 -0700

    function cleanup

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -33,11 +33,8 @@ initialize_system() {
     fi
  
    
-    touch active_logs/heart_rate.log
-    touch active_logs/temperature.log
-    touch active_logs/water_usage.log
     echo ""
-    echo "  [OK] Log files initialized in active_logs/."
+    echo "  [OK] project initialized successfully..."
 }
 
 
