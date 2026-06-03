51257889f8f0a15bd229c69d217e3cd20ac8d6a7
kbd-tech <c.niyonkuru1@alustudent.com>
2026-06-03 12:29:22 +0200

    Update hospital_archive.sh

--- hospital_archive.sh
+++ hospital_archive.sh
@@ -7,7 +7,7 @@ mv active_logs/temperature.log archived_logs/temperature_${TIMESTAMP}.log
 mv active_logs/water_usage.log archived_logs/water_usage_${TIMESTAMP}.log
 
 touch active_logs/heart_rate.log touch
-active_logs/temperature.log
+touch active_logs/temperature.log
 touch active_logs/water_usage.log
 
 echo "Logs archived successfully at ${TIMESTAMP}
