8e9381032fecb4a0b18d5b5f763525eb773a78dd
cniyonkuru1-jpg <c.niyonkuru1@alustudent.com>
2026-06-03 03:27:35 -0700

    Added touch to recreate empty log files

--- hospital_archive.sh
+++ hospital_archive.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
 TIMESTAMP=$(date +%Y%m%d_%H%M)
+
 mv active_logs/heart_rate.log archived_logs/heart_rate_${TIMESTAMP}.log
 mv active_logs/temperature.log archived_logs/temperature_${TIMESTAMP}.log
 mv active_logs/water_usage.log archived_logs/water_usage_${TIMESTAMP}.log
-echo "Logs archived successfully at ${TIMESTAMP}"
+
+touch active_logs/heart_rate.log touch
+active_logs/temperature.log
+touch active_logs/water_usage.log
+
+echo "Logs archived successfully at ${TIMESTAMP}
