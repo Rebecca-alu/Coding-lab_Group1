14ece07a073a036e84e3c1c18ca2ee3dd2a03fac
cniyonkuru1-jpg <c.niyonkuru1@alustudent.com>
2026-06-03 03:34:58 -0700

    add the changes here

--- hospital_archive.sh
+++ hospital_archive.sh
@@ -6,8 +6,8 @@ mv active_logs/heart_rate.log archived_logs/heart_rate_${TIMESTAMP}.log
 mv active_logs/temperature.log archived_logs/temperature_${TIMESTAMP}.log
 mv active_logs/water_usage.log archived_logs/water_usage_${TIMESTAMP}.log
 
-touch active_logs/heart_rate.log touch
-active_logs/temperature.log
+touch active_logs/heart_rate.log 
+touch active_logs/temperature.log
 touch active_logs/water_usage.log
 
 echo "Logs archived successfully at ${TIMESTAMP}
