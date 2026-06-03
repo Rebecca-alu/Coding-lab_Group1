ffbec521001395d5b7635915c72437bf54c0e5d4
mynameisedwinjust <e.muhire@alustudent.com>
2026-06-03 07:25:30 -0700

    water audit summary function implemented

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -14,3 +14,51 @@ process_vitals() {
 
     echo "Report created for CRITICAL Vitals"
 }
+
+water_audit() {
+    log_file="active_logs/water_usage_log.log"
+
+    if [ ! -f active_logs ]; then
+        echo "The logs directory is missing"
+        return 1
+    fi
+
+    average=$(awk -F' \\| ' '
+        $2 == "ICU_WATER_RESERVE" {
+            sum += $3
+            count++
+        }
+        END {
+            if (count > 0)
+                print sum / count
+            else
+                print 0
+        }
+    ' "$log_file")
+
+    total_records=$(awk -F' \\| ' '
+        $2 == "ICU_WATER_RESERVE" {
+            count++
+        }
+        END {
+            print count
+        }
+    ' "$log_file")
+
+    printf "\n"
+    printf "\n"
+    printf "water audit  REPORT summary\n"
+    printf "\n"
+    printf "%-20s : %s\n" "Resource" "ICU_WATER_RESERVE"
+    printf "%-20s : %d\n" "Records Analysed" "$total_records"
+    printf "%-20s : %.2f Litres\n" "Average Usage" "$average"
+    printf "%-20s : %s\n" "Generated" "$(date)"
+    printf "===============================================\n"
+}
+
+
+# Executing our functions
+
+process_vitals
+echo ""
+water_audit
