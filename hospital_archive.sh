9cc0acd7a46575814842ae538d398704ce67924d
cniyonkuru1-jpg <c.niyonkuru1@alustudent.com>
2026-06-03 02:44:06 -0700

    Created hospital_archive.sh script

---
+++ hospital_archive.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+TIMESTAMP=$(date +%Y%m%d_%H%M)
+mv active_logs/heart_rate.log archived_logs/heart_rate_${TIMESTAMP}.log
+mv active_logs/temperature.log archived_logs/temperature_${TIMESTAMP}.log
+mv active_logs/water_usage.log archived_logs/water_usage_${TIMESTAMP}.log
+echo "Logs archived successfully at ${TIMESTAMP}"
