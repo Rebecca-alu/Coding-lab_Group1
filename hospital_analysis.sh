e19234b08844525b54c10c2293be3fec3320a4bd
acyubahiroo <a.cyubahiro@alustudent.com>
2026-06-03 15:58:17 +0200

    Fix typo in vital analysis report filename

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 process_vitals() {
 
-   	echo "Analyzing critical vitals..."
+   	echo "Analyzing critical vitals....."
     	> reports/critical_alerts.txt
 
 	for file in heart_rate_log temperature_log; do
 
     		grep "CRITICAL" active_logs/"$file".log \
 		| awk -F',' '{print $1","$2","$3}' \
-    		>> reports/critical_alert.txt
+    		>> reports/critical_alerts.txt
 	done
 
     echo "Report created for CRITICAL Vitals"
